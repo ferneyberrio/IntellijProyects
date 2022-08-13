f47fe6025d1791b78cf2af8db00197351cb3b4b0
ferneyberrio <fabz10xx@gmail.com>
2022-08-13 11:26:39 -0500

    add method mensaje

--- AplicacionUnoRep/src/AppUnoRep.java
+++ AplicacionUnoRep/src/AppUnoRep.java
@@ -1,3 +1,7 @@
 public class AppUnoRep {
+    private String mensaj = "un método ";
+     void mensaje(String msm){
+        System.out.println(msm + mensaj);
+    }// fin metodo
 
-}
+}// fin clase
--- AplicacionUnoRep/src/Main.java
+++ AplicacionUnoRep/src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hola Mundo!");
-
+        AppUnoRep msm2 = new AppUnoRep();
+        msm2.mensaje("estoy en ");
     }// fin main
 }// fin class main
